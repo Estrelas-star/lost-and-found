e81198589d3bc07b2e7364c90215c2a503cd4ceb
Silmy080712txy <3496431358@qq.com>
2026-09-23 11:47:29 +0800

    build: 新增开发服务器代理以规避跨域

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://111.229.234.32:8080',
+        changeOrigin: true
+      }
+    }
+  }
 })
